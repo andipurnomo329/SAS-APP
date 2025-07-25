5f055774f39223700b98749ad272030e760c84df
Andi Purnomo <andipurnomo329@gmail.com>
2025-07-25 14:47:17 +0700

    Add Boostrap

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,19 @@
+    <footer class="footer">
+        <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2023 <a
+                    href="https://www.bootstrapdash.com/" target="_blank">BootstrapDash</a>. All rights reserved.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i
+                    class="mdi mdi-heart text-danger"></i></span>
+        </div>
+    </footer>
+
+    <script src="{{ asset('vendors/js/vendor.bundle.base.js') }}"></script>
+    <script src="{{ asset('vendors/chart.js/chart.umd.js') }}"></script>
+    <script src="{{ asset('assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
+
+    <script src="{{ asset('js/off-canvas.js') }}"></script>
+    <script src="{{ asset('js/misc.js') }}"></script>
+    <script src="{{ asset('js/settings.js') }}"></script>
+    <script src="{{ asset('js/todolist.js') }}"></script>
+    <script src="{{ asset('js/jquery.cookie.js') }}"></script>
+    <script src="{{ asset('js/dashboard.js') }}"></script>
